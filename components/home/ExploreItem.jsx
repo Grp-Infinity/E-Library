8d60128a3b2340558d87fb029aad674a361b3eb5
Maleesha Dinuranga <35028761+dinuranga@users.noreply.github.com>
2024-07-02 02:31:27 +0530

    Update saved

--- components/home/ExploreItem.jsx
+++ components/home/ExploreItem.jsx
@@ -1,17 +1,8 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import ExploreBook from "./ExploreBook";
 
-const ExploreItem = ({ book }) => {
-  return (
-    <View style={{ flexDirection: "row", padding: 10, alignItems: "center" }}>
-      <Image source={book.image} style={{ width: 50, height: 50, marginRight: 10 }} />
-      <View>
-        <Text style={{ fontSize: 16 }}>{book.title}</Text>
-        <Text style={{ fontSize: 14, color: "gray" }}>{book.author}</Text>
-        <Text style={{ fontSize: 14 }}>{`Rating: ${book.rating}`}</Text>
-      </View>
-    </View>
-  );
+const ExploreItem = ({ book, openPdf }) => {
+  return <ExploreBook book={book} openPdf={openPdf} />;
 };
 
 export default ExploreItem;
